4b40e1f8ba7f044912fc370dcc91c0b97cf22ad8
Brett Lawson <brett19@gmail.com>
2020-07-28 13:27:01 -0700

    Moved staged mutations to their own file.

--- transactionattempt.go
+++ transactionattempt.go
@@ -39,43 +39,6 @@ const (
 	AttemptStateRolledBack = AttemptState(6)
 )
 
-// StagedMutationType represents the type of a mutation performed in a transaction.
-type StagedMutationType int
-
-const (
-	// StagedMutationInsert indicates the staged mutation was an insert operation.
-	StagedMutationInsert = StagedMutationType(1)
-
-	// StagedMutationReplace indicates the staged mutation was an replace operation.
-	StagedMutationReplace = StagedMutationType(2)
-
-	// StagedMutationRemove indicates the staged mutation was an remove operation.
-	StagedMutationRemove = StagedMutationType(3)
-)
-
-// StagedMutation wraps all of the information about a mutation which has been staged
-// as part of the transaction and which should later be unstaged when the transaction
-// has been committed.
-type StagedMutation struct {
-	OpType         StagedMutationType
-	BucketName     string
-	ScopeName      string
-	CollectionName string
-	Key            []byte
-	Cas            gocbcore.Cas
-	Staged         json.RawMessage
-}
-
-type stagedMutation struct {
-	OpType         StagedMutationType
-	Agent          *gocbcore.Agent
-	ScopeName      string
-	CollectionName string
-	Key            []byte
-	Cas            gocbcore.Cas
-	Staged         json.RawMessage
-}
-
 type transactionAttempt struct {
 	// immutable state
 	expiryTime      time.Time
